b8e6817aa40be6a593a7d8f012fb8ee90ec1e195
Rodrigo Brunhara <rodrigo.brunhara@hotmail.com>
2023-07-24 18:04:26 -0300

    ci

--- scripts/pullRequestPreview.js
+++ scripts/pullRequestPreview.js
@@ -1,1 +1,6 @@
-console.log("Olá mundo! :)");
+import { execSync } from "child_process";
+console.log("[DEPLOY_PREVIEW]: START");
+const command = "yarn deploy:staging";
+const output = execSync(command, { encoding: "utf-8" });
+console.log(output);
+console.log("[DEPLOY_PREVIEW]: end");
