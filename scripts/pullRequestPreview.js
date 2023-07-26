9e10f3742bb7bd10df1ba327ef4792bc2698542d
Rodrigo Brunhara <rodrigo.brunhara@hotmail.com>
2023-07-26 17:46:58 -0300

    teste

--- scripts/pullRequestPreview.js
+++ scripts/pullRequestPreview.js
@@ -27,7 +27,7 @@ defaultHeaders["content-type"] = "application/json";
 
 console.log("GITHUB_REPOSITORY", GITHUB_REPOSITORY);
 console.log("GITHUB_PR_NUMBER", GITHUB_PR_NUMBER);
-
+console.log("GITHUB_TOKEN", GITHUB_TOKEN);
 fetch(
   `https://api.github.com/repos/${GITHUB_REPOSITORY}/issues/${GITHUB_PR_NUMBER}/comments`,
   {
